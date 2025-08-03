104c6fdf62bf88bd16b9ec5a2643592ed53e50de
Tamaki Mizuno <tamaki.mizuno@gmail.com>
2025-08-03 23:09:38 +0900

    課題３を追加

--- myFirstStrudel.js
+++ myFirstStrudel.js
@@ -1,3 +1,5 @@
+//練習課題 1: 基本的なビートの構築とテンポ調整
+//練習課題 2: 変数を使った整理とベースラインの追加
 setCpm(115/4)
 
 const drums = sound("bd sd bd sd").bank("RolandTR909")
@@ -9,7 +11,18 @@ const bass = n("0 4 2 4")
   .n("<32 16 8 4>")
   .scope()
 
+_$: drums
+_$: hats
+_$: bass
 
-$: drums
-$: hats
+//練習課題 3: バリエーションの導入とシーケンスの応用
+const mainBeat = s("bd sd bd sd*2").bank("RolandTR909")
+const fill = s("bd*8 sd").bank("RolandTR909")
+
+const variedDrums = every(4, () => fill, mainBeat)
+
+const complexHats = s("hh <[hh hh]*3 hh>").bank("RolandTR909").gain("0.2")
+
+$: variedDrums
+$: complexHats
 $: bass
