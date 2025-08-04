875ba74abfc95b4325766d85840d0cea8d0ef202
Tamaki Mizuno <tamaki.mizuno@gmail.com>
2025-08-04 22:33:48 +0900

    Create crow.js

---
+++ crow.js
@@ -0,0 +1,9 @@
+// Hello World!
+// this is @mizu__tama.
+// its my 1st livecoding video.
+
+stack(
+  n("- - 0 -").sound("crow").mask("<0 1>").color("blue")
+  , s("bd <sd [sd clap]> bd sd").color("cyan")
+  , n("0 - 2 1").scale("C2:minor").sound("sawtooth")
+).scope()
