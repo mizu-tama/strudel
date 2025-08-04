f91659dbee1f26e69a999cdb19ecbd22c10de148
Tamaki Mizuno <tamaki.mizuno@gmail.com>
2025-08-04 14:05:21 +0900

    Update bass in myFirstStrudel.js

--- myFirstStrudel.js
+++ myFirstStrudel.js
@@ -5,7 +5,7 @@ setCpm(115/4)
 const drums = sound("bd sd bd sd").bank("RolandTR909")
 const hats = sound("hh*16").bank("RolandTR909").gain("0.2 0.5")
 
-const bass = n("0 4 2 4")
+const bass = n("0 4 2 -")
   .scale("C:major")
   .sound("<sawtooth square> triangle")
   .n("<32 16 8 4>")
