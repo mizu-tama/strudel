30fd18bf81773875f48e75d1494ef4e245c39b2f
Tamaki Mizuno <tamaki.mizuno@gmail.com>
2025-08-08 09:49:17 +0900

    Create ttsl.js

---
+++ TTLS/ttsl.js
@@ -0,0 +1,19 @@
+// practice notes
+// Twinkl Twinkl Little Star
+
+setcps(120/60/8)
+$:note(`
+     <
+     [c c g g a a g -]
+     [f f e e d d c -]
+     [g g f f e e d -]!2
+     [c c g g a a g -]
+     [f f e e d d c -]
+     >
+  `).sound("piano")
+  ._punchcard()
+
+
+$:n("0").sound("gm_lead_3_calliope")
+
+     
